cd93cecca128cbeee439b6992969e6a0c7d9e69f
delphist <xdelphist@gmail.com>
2021-02-05 20:45:27 +0800

    Fix some vcr errors

--- app/api/public/helpers/contentful_helper.rb
+++ app/api/public/helpers/contentful_helper.rb
@@ -4,6 +4,7 @@ module Public
   module Helpers
     module ContentfulHelper
       CONTENTFUL_CLIENT = Contentful::Client.new(
+        # Note: this important data (token) is exposed here to simplify installation process
         access_token: ENV.fetch('CONTENTFUL_TOKEN', '7ac531648a1b5e1dab6c18b0979f822a5aad0fe5f1109829b8a197eb2be4b84c'),
         space: ENV.fetch('CONTENTFUL_SPACE', 'kk2bw5ojx476'),
         environment: ENV.fetch('CONTENTFUL_ENVIRONMENT', 'master')
--- spec/support/vcr_helper.rb
+++ spec/support/vcr_helper.rb
@@ -1,10 +1,14 @@
 # frozen_string_literal: true
 
 require 'vcr'
+require 'uri'
 
 VCR.configure do |c|
   c.ignore_hosts '127.0.0.1', 'localhost'
   c.cassette_library_dir = 'spec/vcr'
   c.hook_into :webmock
   c.configure_rspec_metadata!
+
+  driver_hosts = Webdrivers::Common.subclasses.map { |driver| URI(driver.base_url).host }
+  c.ignore_hosts(*driver_hosts)
 end
